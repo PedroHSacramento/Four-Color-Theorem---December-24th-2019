4f6cc3ad4d5cbc84e8d71582605728267b365cfc
hjamal3 <hjamal@gatech.edu>
2019-06-04 17:33:17 -0400

    Fix

--- graph.c
+++ graph.c
@@ -14,7 +14,7 @@ struct edge {
 	struct edge* rev;		// reverse edge
 	int color;
 };
-
+//
 struct vertex {
 	int num;
 	int deg;
@@ -50,6 +50,30 @@ void my_free(void* size){
 static void error(){
 	ERROR = true;
 	printf("%s",error_msg);
+}
+
+// DFS to color the vertices
+static void color_dfs(struct vertex* v, int* colored) {
+	struct edge * e = v->e;
+	int i;
+	for(i = 0; i < v->deg; i++){
+		if( colored[e->rev->v->num] == -1 ){
+			colored[e->rev->v->num] = (colored[v->num])^(e->color);
+			color_dfs(e->rev->v, colored);
+		}
+		e = e->next;
+	}
+}
+
+// colors edges of graph
+static void color_vertices(struct graph* g) {
+	int i;
+	int* colored = (int*) malloc(sizeof(int) * (g->n + 1));
+	for(i = 0; i <= g->n; i++) colored[i] = -1;
+	colored[g->vert->num] = 0;
+	color_dfs(g->vert, colored);
+	for(i = 1; i <= g->n; i++) printf("%d:%d ", i, colored[i]);
+	printf("\n");
 }
 
 // finds edge between v and u in the list of edges of v
@@ -129,7 +153,7 @@ static struct graph* read_graph(char *data){
 	struct vertex* v2;
 	struct edge* e;
 	FILE* ptr = fopen(data,"r");
-	
+
 	fscanf(ptr,"%d",&g->n);
 	v1 = (struct vertex*) malloc(sizeof(struct vertex));
 	if(v1 == NULL){
@@ -187,7 +211,80 @@ static struct graph* read_graph(char *data){
 	return g;
 }
 
-// frees the whole graph
+// For debugging: read a graph with a fixed tri coloring
+static struct graph* read_graph_colored(char *data){
+
+	// first line is the number of vertices
+	// second line is list of degrees of each vertex
+	// subsequent lines (starting from i=0)
+  	// are the vertices that vertex i is adjacent to
+
+	int i, j, u;
+	struct graph* g = (struct graph*) malloc(sizeof(struct graph));
+	struct vertex* v1;
+	struct vertex* v2;
+	struct edge* e;
+
+	FILE* ptr = fopen(data,"r");
+	fscanf(ptr,"%d",&g->n);
+
+	v1 = (struct vertex*) malloc(sizeof(struct vertex));
+	if(v1 == NULL){
+		strcpy(error_msg, "Memory error: not enough space for new vertex in function read_graph\n");
+		error();
+		return NULL;
+	}
+	g->vert = v1;
+	v1->num = 1;
+	for(i = 2; i <= g->n; i++){
+		v2 = (struct vertex*) malloc(sizeof(struct vertex));
+		if(v2 == NULL){
+			strcpy(error_msg, "Memory error: not enough space for new vertex in function read_graph\n");
+			error();
+			return NULL;
+		}
+		v2->num = i;
+		v1->next = v2;
+		v2->prev = v1;
+		v1 = v2;
+	}
+	v2->next = g->vert;
+	g->vert->prev = v2;
+
+	v1 = g->vert;
+	for(i = 1; i <= g->n; i++){
+		v1->e = NULL;
+		fscanf(ptr, "%d", &v1->deg);
+		if(v1->deg < 0){
+			strcpy(error_msg, "Invalid degree, must be >= 0\n");
+			error();
+			return NULL;
+		}
+		v1 = v1->next;
+	}
+
+	v1 = g->vert;
+	for(i = 1; i <= g->n; i++){
+		for(j = 0; j < v1->deg; j++){
+			fscanf(ptr,"%d",&u);
+			if(u <= 0 && u > g->n){
+				strcpy(error_msg, "Invalid vertex, must be between 1 and n\n");
+				error();
+				return NULL;
+			}
+			v2 = v1->next;
+			while(v2->num != u){
+				v2 = v2->next;
+			}
+			add_edge(v1, v2);
+			fscanf(ptr,"%d", &v1->e->prev->color);
+		}
+		v1 = v1->next;
+	}
+	return g;
+}
+
+// frees the whole graph // buggy
 static void free_graph(struct graph* g){
 	struct vertex* v = g->vert;
 	struct edge* e;
@@ -196,16 +293,19 @@ static void free_graph(struct graph* g){
 	for(i = 0; i < g->n; i++){
 		e =  v->e;
 		for(j = 1; j < v->deg; j++){
+			debug("free edge %d %d\n",e->prev->v->num,e->prev->rev->v->num);
 			free(e->prev);
+			e = e->next;
 		}
 		free(e);
 		v = v->next;
 	}
 	// frees the vertices
 	for(i = 1; i < g->n; i++){
-		free(v->prev);
+//		free(v->prev);
+		v = v->next;
 	}
-	free(v);
+//	free(v);
 	// frees the graph
 	free(g);
 }
@@ -246,8 +346,10 @@ int main(){
 	printf("Enter file name including extension: \n");
 	scanf("%s", data);
 	g = read_graph(data);
+	//g = read_graph_colored(data); // for debugging
 	print_graph(g);
-	free_graph(g);
-	printf("Leaking memory: %d\n",memory_in_use);
+	//color_vertices(g); // final step
+	//free_graph(g); // currently buggy
+	//printf("Leaking memory: %d\n",memory_in_use);
 	return 0;
 }
